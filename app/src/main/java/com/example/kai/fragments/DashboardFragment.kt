809374cd68ec1e57044ac2c2e1789ca3146550c2
Zaddy1 <115181439+Zaddy1@users.noreply.github.com>
2022-12-13 23:18:33 +0400

    Initial commit

---
+++ app/src/main/java/com/example/kai/fragments/DashboardFragment.kt
@@ -0,0 +1,12 @@
+package com.example.kai.fragments
+
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.example.kai.R
+
+class DashboardFragment: Fragment(R.layout.dashboard_fragment) {
+}
---
+++ app/src/main/java/com/example/kai/fragments/SettingsFragment.kt
@@ -0,0 +1,7 @@
+package com.example.kai.fragments
+
+import androidx.fragment.app.Fragment
+import com.example.kai.R
+
+class SettingsFragment: Fragment(R.layout.settings_fragment) {
+}
